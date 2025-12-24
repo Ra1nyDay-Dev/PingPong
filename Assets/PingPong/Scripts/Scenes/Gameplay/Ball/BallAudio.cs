5796690d48df0aaa0ef4a9340da9d519c0cc57f5
Щупаков Роман <Dante967@mail.ru>
2025-12-24 13:39:34 +0300

    feat: звуки отскока мяча и гола

---
+++ Assets/PingPong/Scripts/Scenes/Gameplay/Ball/BallAudio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace PingPong.Scripts.Scenes.Gameplay.Ball
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class BallAudio : MonoBehaviour
+    {
+        [SerializeField] private AudioClip[] _ballHitSounds;
+        [SerializeField] private float _minPitch = 1f;
+        [SerializeField] private float _maxPitch = 1.2f;
+        
+        private AudioSource _audioSource;
+
+        private void Awake() => 
+            _audioSource = gameObject.GetComponent<AudioSource>();
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Paddle") || other.gameObject.CompareTag("Wall"))
+            {
+                _audioSource.pitch = Random.Range(_minPitch, _maxPitch);
+                var ballHitClip = _ballHitSounds[Random.Range(0, _ballHitSounds.Length)];
+                _audioSource.PlayOneShot(ballHitClip);
+            }
+        }
+    }
+}
---
+++ Assets/PingPong/Scripts/Scenes/Gameplay/Gate/GateAudio.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace PingPong.Scripts.Scenes.Gameplay.Gate
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class GateAudio : MonoBehaviour
+    {
+        [SerializeField] private AudioClip _goalAlarm;
+        [SerializeField] private AudioClip _goalHit;
+        
+        private AudioSource _audioSource;
+
+        private void Awake() => 
+            _audioSource = gameObject.AddComponent<AudioSource>();
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Ball"))
+            {
+                _audioSource.PlayOneShot(_goalAlarm);
+                _audioSource.PlayOneShot(_goalHit);
+            }
+        }
+    }
+}
