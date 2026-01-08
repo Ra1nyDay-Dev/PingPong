bb1860092de46aa701039d071f8cb0397b8b4fe7
Щупаков Роман <Dante967@mail.ru>
2026-01-08 04:31:14 +0300

    feat: AudioMixer, регулировка громкости звуков и музыки с запоминанием выбора

---
+++ Assets/PingPong/Scripts/Global/Services/GameAudioMixer/IGameAudioMixer.cs
@@ -0,0 +1,10 @@
+﻿namespace PingPong.Scripts.Global.Services.GameAudioMixer
+{
+    public interface IGameAudioMixer
+    {
+        void GetMusicVolume();
+        void SetMusicVolume(float volume);
+        void GetSFXVolume();
+        void SetSFXVolume(float volume);
+    }
+}
